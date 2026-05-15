7bd9cfbfb505b533b45a489bf2fb72e49d110f7c
Gabedf <gabdwork@gmail.com>
2026-05-14 21:26:13 -0300

    starting projetct

---
+++ src/main/java/GabBank/GabankApplication.java
@@ -0,0 +1,13 @@
+package GabBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GabBank/GabankApplicationTests.java
@@ -0,0 +1,13 @@
+package GabBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
